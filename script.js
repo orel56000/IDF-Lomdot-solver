50388c482928ec84fc6beedcc33a0f1119c7542a
orel56000 <54208288+orel56000@users.noreply.github.com>
2022-05-14 08:27:42 +0300

    Main script

---
+++ script.js
@@ -0,0 +1,34 @@
+const questionList = document.getElementsByClassName("questionList"); // Get all questions.
+
+for(let i = 0; i < questionList.length; i++) // Loop all questions.
+{
+	for(let j = 0; j < questionList[i].children.length; j++) // Loop all potential answers.
+	{
+		for(let h = 0; h < questionList[i].children[j].getElementsByClassName("labelWrapper").length; h++) // Get answer itself.
+		{
+			if(questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].dataset.isCorrect == "True") // If the answer has isCorrect dataset.
+			{
+				console.log("True "+h);
+				questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].click(); // Select the answer.
+			}
+			else
+				console.log("false "+h); // Don't select, just move to the next one.
+		}
+		
+	}
+	
+}
+
+// Create an interval to press next.
+setInterval(() => {
+	const loaderHolder = document.getElementById("loaderHolder");
+	console.log(loaderHolder == null);
+	if(loaderHolder == null)
+	{
+		const nextPage = document.getElementsByClassName("surveyBtn js-submitBtn nextPage")[0];
+		nextPage.click();
+		console.log("pressed next");
+	}
+	
+		
+}, 1000)
