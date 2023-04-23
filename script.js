e18bb1a7c17e6aaf895c7339e5f6770fa5e85ee7
orel56000 <54208288+orel56000@users.noreply.github.com>
2023-04-23 09:19:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
 const questionList = document.getElementsByClassName("questionList"); // Get all questions.
+const nextPage = document.getElementsByClassName("surveyBtn js-submitBtn nextPage")[0] || document.getElementsByClassName("surveyBtn js-submitBtn finalPage")[0];
 
-for(let i = 0; i < questionList.length; i++) // Loop all questions.
-{
-	for(let j = 0; j < questionList[i].children.length; j++) // Loop all potential answers.
+const solvePage = () => {
+	for(let i = 0; i < questionList.length; i++) // Loop all questions.
 	{
-		for(let h = 0; h < questionList[i].children[j].getElementsByClassName("labelWrapper").length; h++) // Get answer itself.
+		for(let j = 0; j < questionList[i].children.length; j++) // Loop all potential answers.
 		{
-			if(questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].dataset.isCorrect == "True") // If the answer has isCorrect dataset.
+			for(let h = 0; h < questionList[i].children[j].getElementsByClassName("labelWrapper").length; h++) // Get answer itself.
 			{
-				console.log("True "+h);
-				questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].click(); // Select the answer.
+				if(questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].dataset.isCorrect == "True") // If the answer has isCorrect dataset.
+				{
+					console.log("True "+h);
+					questionList[i].children[j].getElementsByClassName("labelWrapper")[h].children[0].children[0].click(); // Select the answer.
+				}
+				else
+					console.log("false "+h); // Don't select, just move to the next one.
 			}
-			else
-				console.log("false "+h); // Don't select, just move to the next one.
+			
 		}
 		
 	}
-	
+
+	// Create an interval to press next.
+	setInterval(() => {
+		const loaderHolder = document.getElementById("loaderHolder");
+		console.log(loaderHolder == null);
+		if(loaderHolder == null)
+		{
+			
+			nextPage.click();
+			console.log("pressed next");
+		}
+		
+			
+	}, 1000)
 }
 
-// Create an interval to press next.
-setInterval(() => {
-	const loaderHolder = document.getElementById("loaderHolder");
-	console.log(loaderHolder == null);
-	if(loaderHolder == null)
-	{
-		const nextPage = document.getElementsByClassName("surveyBtn js-submitBtn nextPage")[0];
-		nextPage.click();
-		console.log("pressed next");
-	}
-	
-		
-}, 1000)
+if (nextPage != null) {
+	solvePage();
+} else {
+	console.log("Not a question page");
+}
+
+
+console.log("Hey from lomdot solver");
